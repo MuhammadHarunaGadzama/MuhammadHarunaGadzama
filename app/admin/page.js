9fa9cce0693d884960c9120772bc664016db50b3
MuhammadHarunaGadzama <muhammadhg2048@gmail.com>
2026-05-16 22:53:43 +0100

    app/admin/page.jsx

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,17 +1,74 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function AdminPage() {
-  const students = ["CBT001", "CBT002", "CBT003"];
+  const router = useRouter();
+
+  useEffect(() => {
+    const role = localStorage.getItem("role");
+
+    if (role !== "admin") {
+      router.push("/");
+    }
+  }, []);
 
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>Admin Dashboard</h1>
+    <div className="min-h-screen bg-gray-100 p-10">
 
-      <h3>Registered Students</h3>
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
 
-      {students.map((s) => (
-        <div key={s} style={{ padding: "10px", border: "1px solid #ccc", marginTop: "10px" }}>
-          {s}
+        <h1 className="text-4xl font-bold text-blue-700">
+          Admin Dashboard
+        </h1>
+
+        <div className="mt-10 grid md:grid-cols-3 gap-5">
+
+          <div className="bg-blue-100 p-6 rounded-xl">
+            <h2 className="text-xl font-bold">
+              Total Questions
+            </h2>
+
+            <p className="text-3xl mt-3">
+              50
+            </p>
+          </div>
+
+          <div className="bg-green-100 p-6 rounded-xl">
+            <h2 className="text-xl font-bold">
+              Exam Duration
+            </h2>
+
+            <p className="text-3xl mt-3">
+              40 Minutes
+            </p>
+          </div>
+
+          <div className="bg-yellow-100 p-6 rounded-xl">
+            <h2 className="text-xl font-bold">
+              Admin Access
+            </h2>
+
+            <p className="text-3xl mt-3">
+              Granted
+            </p>
+          </div>
+
         </div>
-      ))}
+
+        <button
+          onClick={() => {
+            localStorage.clear();
+            router.push("/");
+          }}
+          className="mt-10 bg-red-600 text-white px-6 py-3 rounded-xl"
+        >
+          Logout
+        </button>
+
+      </div>
+
     </div>
   );
-}
+    }
